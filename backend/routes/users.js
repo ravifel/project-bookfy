e357228e1c0a51a734dcdc96f37aeefff8635991
Ravi Silva <ravifelipee@gmail.com>
2025-01-14 08:28:49 -0300

    implementing update method

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,16 +11,21 @@ router.get('/', (request, response) =>
         .then(users => {
             console.log(users);
             response.sendStatus(200);
+            response.send('users', {
+                users
+            })
         })
         .catch(err => console.log(err)
         ));
 
+// Get user by 'id'
+
 // Add a user
 router.get('/add', (request, response) => {
     const data = {
-        name: 'Thales Isaac Vinicius Porto',
-        cpf: '20427978718',
-        email: 'thalesisaacporto@transtelli.com.br',
+        name: 'Thales tESTE',
+        cpf: '20427978799',
+        email: 'thalesteste@transtelli.com.br',
         telephone: '2128900795',
         birthday: moment('02/04/1984', 'DD/MM/YYYY').format('YYYY-MM-DD'),
         password: 'X65o1U7dXa',
@@ -28,7 +33,6 @@ router.get('/add', (request, response) => {
         profile_picture: 'thales-porto.jpg'
     }
 
-    profile_picture: 'diego-mendes.jpg'
     let {
         name,
         cpf,
@@ -54,5 +58,47 @@ router.get('/add', (request, response) => {
         .then(user => response.redirect('/users'))
         .catch(err => console.log(err));
 });
+
+
+// Updating User
+router.get('/update/:id', (request, response) => {
+    const data = {
+        name: 'Thales tESTE 123',
+        cpf: '20427978799',
+        email: 'thalesteste123@transtelli.com.br',
+        telephone: '2128900795',
+        birthday: moment('02/04/1984', 'DD/MM/YYYY').format('YYYY-MM-DD'),
+        password: 'X65o1U7dXa',
+        user_type: 'seller',
+        profile_picture: 'thales-porto.jpg'
+    }
+
+    let {
+        name,
+        cpf,
+        email,
+        telephone,
+        birthday,
+        password,
+        user_type,
+        profile_picture
+    } = data;
+
+    // Insert into table
+    Users.update({
+        name,
+        cpf,
+        email,
+        telephone,
+        birthday,
+        password,
+        user_type,
+        profile_picture
+    })
+        .then(user => response.redirect('/users'))
+        .catch(err => console.log(err));
+});
+
+// Delete User
 
 module.exports = router;
